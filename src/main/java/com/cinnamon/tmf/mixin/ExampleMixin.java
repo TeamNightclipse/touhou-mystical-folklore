539a7b6005481072e58c6c158936068b62361a92
fourh0 <urreaching4h0@gmail.com>
2021-11-24 04:17:14 +0000

    greg

--- src/main/java/com/cinnamon/tmf/mixin/ExampleMixin.java
+++ src/main/java/com/cinnamon/tmf/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package com.example.examplemod.mixin;
+package com.cinnamon.tmf.mixin;
 
-import com.example.examplemod.ExampleMod;
+import com.cinnamon.tmf.TMFMod;
 import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +8,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
-    @Inject(at = @At("HEAD"), method = "init()V")
-    private void init(CallbackInfo info) {
-        ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
-    }
+public class ExampleMixin
+{
+
 }
