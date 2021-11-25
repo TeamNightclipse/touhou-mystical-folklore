8334f008bbabc3da9f03055fafe8c9fcf5e5e23a
ArekkuusuJerii <edgargcx99@gmail.com>
2021-11-24 19:17:35 -0600

    *uwu*

---
+++ fabric/src/main/java/com/cinnamon/tmf/fabric/ExampleModFabric.java
@@ -0,0 +1,11 @@
+package com.cinnamon.tmf.fabric;
+
+import com.cinnamon.tmf.ExampleMod;
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleModFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        ExampleMod.init();
+    }
+}
---
+++ forge/src/main/java/com/cinnamon/tmf/forge/ExampleExpectPlatformImpl.java
@@ -0,0 +1,15 @@
+package com.cinnamon.tmf.forge;
+
+import com.cinnamon.tmf.ExampleExpectPlatform;
+import net.minecraftforge.fml.loading.FMLPaths;
+
+import java.nio.file.Path;
+
+public class ExampleExpectPlatformImpl {
+    /**
+     * This is our actual method to {@link ExampleExpectPlatform#getConfigDirectory()}.
+     */
+    public static Path getConfigDirectory() {
+        return FMLPaths.CONFIGDIR.get();
+    }
+}
